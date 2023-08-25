292d6601019de58e8aa499f153ceae2044e65ae8
dot-D69 <anuragvishwas19@gmail.com>
2023-08-26 00:06:45 +0530

    added the functionality for pswd strength checker

--- main.js
+++ main.js
@@ -19,3 +19,56 @@ const showHiddenPass = (loginPass,loginEye) => {
 }
 
 showHiddenPass('login-password','login-eye')
+
+const passwordStrengthIndicator = document.getElementById("password-strength");
+const strengthText = document.getElementById("strength-text");
+const passwordInput = document.getElementById("login-password");
+
+passwordInput.addEventListener("input", updatePasswordStrength);
+
+function updatePasswordStrength() {
+    const password = passwordInput.value;
+    const strength = calculatePasswordStrength(password);
+    updateStrengthIndicator(strength);
+}
+
+function calculatePasswordStrength(password) {
+    const minLength = 8;
+    const lowercaseRegex = /[a-z]/;
+    const uppercaseRegex = /[A-Z]/;
+    const numbersRegex = /[0-9]/;
+    const specialCharsRegex = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/;
+
+    let score = 0;
+
+    if (password.length >= minLength) {
+        if ((lowercaseRegex.test(password) || uppercaseRegex.test(password)) && password.length < minLength) {
+            score++; // Weak
+        }
+        if (lowercaseRegex.test(password) && uppercaseRegex.test(password) && password.length >= minLength) {
+            score++; // Moderate
+        }
+        if (lowercaseRegex.test(password) && uppercaseRegex.test(password) && numbersRegex.test(password) && password.length >= minLength) {
+            score++; // Strong
+        }
+        if (lowercaseRegex.test(password) && uppercaseRegex.test(password) && numbersRegex.test(password) && specialCharsRegex.test(password) && password.length >= minLength) {
+            score++; // Very Strong
+        }
+    }
+
+    return score;
+}
+
+function updateStrengthIndicator(strength) {
+    const strengthLabels = ["Weak", "Moderate", "Strong", "Very Strong"];
+    const strengthClasses = ["weak", "moderate", "strong", "very-strong"];
+    const strengthIndex = Math.min(Math.floor(strength), strengthLabels.length - 1);
+
+    for (let i = 0; i < strengthClasses.length; i++) {
+        passwordStrengthIndicator.classList.remove(strengthClasses[i]);
+    }
+
+    passwordStrengthIndicator.innerHTML = strengthLabels[strengthIndex];
+    passwordStrengthIndicator.classList.add(strengthClasses[strengthIndex]);
+}
+
